c6c0bc4fd0976bf6981953b754b4d08b44798f02
mazh91 <milad.azh@ryerson.ca>
2017-08-24 04:56:33 -0400

    finalized version ready for submission

--- js/controllers.js
+++ js/controllers.js
@@ -3,48 +3,44 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', ['$scope', '$http', '$interval',
     function ($scope, $http, $interval) {
         // Retrieve data
-        // $http.get('js/data_w3s.json').success(function (data) {
-            // $scope.records = data;
-            // var json_parsed = JSON.parse(localStorage.getItem('ShareData'));
-            // $scope.records = JSON.stringify(json_parsed);
-            $scope.records = JSON.parse(localStorage.getItem('ShareData'));
-            // Sort records by key
-            if ($scope.records.length > 1)
-                $scope.records.sort(function (_x, _y) {
-                    return new Date('2017/08/23 ' + _x) - new Date('2017/08/23 ' + _y);
-                });
-        // });
-        // Handler definition: data transfer to server
+        $scope.load = function () {
+            var _data = localStorage.getItem($scope.load_code);
+            if (_data != null) {
+                $scope.records = JSON.parse(_data);
+                // Sort records by key
+                if ($scope.records.length > 1)
+                    $scope.records.sort(function (_x, _y) {
+                        return new Date('2017/08/23 ' + _x) - new Date('2017/08/23 ' + _y);
+                    });
+            }
+        }
+        $scope.debug = false;
+
         $scope.timer_value = 0;
         $scope.wpm_timer_value = 0;
         var _timer, _wpm_timer, _text;
-        var _doTimeWpm = true;
 
-        //TODO: change timer limit to 60
+        // Handler definition: data transfer to server
         var serverHandler = function () {
             if ($scope.timer_value < 60)
                 $scope.timer_value++;
             else {
                 $interval.cancel(_timer);
-                console.log(">> Sending data..");
+                $interval.cancel(_wpm_timer);
+                _wpm_timer = null;
+                // $scope.wpm_timer_value = 0;
                 analyze();
             }
         }
 
         var wpmHandler = function () {
-            $scope.wpm_timer_value ++;
+            $scope.wpm_timer_value++;
         }
 
         // User interaction handler
         $scope.change = function () {
-            // if (_doTimeWpm) {
-            // _doTimeWpm = true;
-            // $interval.cancel(_wpm_timer);
             if (_wpm_timer == null)
                 _wpm_timer = $interval(wpmHandler, 1000);
-            // }
-            // _doTimeWpm = false;
-
             $scope.timer_value = 0;
             _text = $scope.usertext;
             $interval.cancel(_timer);
@@ -52,53 +48,64 @@ myApp.controller('MyController', ['$scope', '$http', '$interval',
             if (_text.length < 1) {
                 return;
             }
-
-            // if ($scope.timer == 0)
             _timer = $interval(serverHandler, 1000); // call callback fn every 1 sec
         }
 
 
         function analyze() {
             console.log(">> Processing.. "); //
-            var json_obj = {};
+
+            if(typeof($scope.records) == "undefined")
+                $scope.records = {};
             // Time
             var _time = new Date().toLocaleTimeString();
-            json_obj[_time] = {};
+            $scope.records[_time] = {};
 
             // Word Count
             var _words = _text.split(/[\W]+/);
             var _word_count = _words.length;
-            json_obj[_time]["wc"] = _word_count;
+            $scope.records[_time]["wc"] = _word_count;
 
             // WPM
             var _total_minutes = $scope.wpm_timer_value / 60;
             var _char_count = _text.length;
-            var _wpm = (_char_count / 5.0) / (_total_minutes-1);
+            var _wpm = (_char_count / 5.0) / (_total_minutes - 1);
             _wpm = Math.round(_wpm);
-            json_obj[_time]["wpm"] = _wpm;
+            $scope.records[_time]["wpm"] = _wpm;
+            $scope.wpm_timer_value = 0;
 
             // Word Frequency
             var freq_map = {};
             var _tfw = "";
-            _words.forEach( function(_word) {
+            _words.forEach(function (_word) {
                 var _w = _word.toLowerCase();
-                // Top frequency word
-                if (_tfw == "")
-                    _tfw = _w;
-                else if(freq_map[_w] && freq_map[_w] > freq_map[_tfw])
-                    _tfw = _w;
-                // Frequency map construction
-                if(freq_map[_w])
-                    freq_map[_w] ++;
+              // Frequency map construction
+                if (freq_map[_w])
+                    freq_map[_w]++;
                 else
                     freq_map[_w] = 1;
             });
-            json_obj[_time]["tfw"] = _tfw;
+
+            // Top frequency word II
+            var max = 0;
+            for(var key in freq_map){
+                if(max < freq_map[key]) {
+                    max = freq_map[key];
+                    _tfw = key;
+                }
+            }
+
+            $scope.records[_time]["tfw"] = _tfw;
             // console.log(_time + "| " + _word_count + "| " + _wpm + "| " + tfw);
-            // console.log(JSON.stringify(json_obj));
-            localStorage.setItem("ShareData", JSON.stringify(json_obj));
+            // console.log(JSON.stringify($scope.records));
+
         }
-    }]);
+
+        $scope.share = function () {
+            $scope.share_code = Math.random().toString(36).slice(8);
+            localStorage.setItem($scope.share_code, JSON.stringify($scope.records));
+        }
+    }]);    // End of SCOPE
 
 // Custom directive
 // Reference: https://stackoverflow.com/questions/30946624/how-to-use-ngchange-on-a-div-or-something-similar
