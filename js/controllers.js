55a1ebdfb5c761789fbe635cff1c24ad71fcc293
mazh91 <milad.azh@ryerson.ca>
2017-09-28 02:14:19 -0400

    - Added submit button - Preserved input line feed - Fix wpm miscalculation

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,6 @@
+const TIMEOUT_SECONDS = 10;
+const UI_TIMER_MILLISECONDS = 1000;
+const MINUTE_IN_SECONDS = 60;
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', ['$scope', '$http', '$interval',
@@ -18,17 +21,19 @@ myApp.controller('MyController', ['$scope', '$http', '$interval',
 
         $scope.timer_value = 0;
         $scope.wpm_timer_value = 0;
+        $scope.text_input = "";
         var _timer, _wpm_timer, _text;
+        var auto_factor = 1;
 
         // Handler definition: data transfer to server
         var serverHandler = function () {
-            if ($scope.timer_value < 60)
+            if ($scope.timer_value < TIMEOUT_SECONDS)
                 $scope.timer_value++;
             else {
+                auto_factor = 1;
                 $interval.cancel(_timer);
                 $interval.cancel(_wpm_timer);
                 _wpm_timer = null;
-                // $scope.wpm_timer_value = 0;
                 analyze();
             }
         }
@@ -40,36 +45,41 @@ myApp.controller('MyController', ['$scope', '$http', '$interval',
         // User interaction handler
         $scope.change = function () {
             if (_wpm_timer == null)
-                _wpm_timer = $interval(wpmHandler, 1000);
+                _wpm_timer = $interval(wpmHandler, UI_TIMER_MILLISECONDS);
             $scope.timer_value = 0;
-            _text = $scope.usertext;
+            _text = $scope.text_input;
             $interval.cancel(_timer);
 
             if (_text.length < 1) {
                 return;
             }
-            _timer = $interval(serverHandler, 1000); // call callback fn every 1 sec
+            _timer = $interval(serverHandler, UI_TIMER_MILLISECONDS);
         }
 
 
         function analyze() {
             console.log(">> Processing.. "); //
+            $scope.timer_value = null;
+            $scope.text_input = "";
 
-            if(typeof($scope.records) == "undefined")
+            if(typeof($scope.records) == "undefined") {
                 $scope.records = {};
+            }
             // Time
             var _time = new Date().toLocaleTimeString();
-            $scope.records[_time] = {};
 
+            $scope.records[_time] = {};
             // Word Count
-            var _words = _text.split(/[\W]+/);
-            var _word_count = _words.length;
+            var _words = _text.replace( /\n/g, " " ).split(/[\W]+/);
+            var _word_count = _words.length - 1;
             $scope.records[_time]["wc"] = _word_count;
 
             // WPM
-            var _total_minutes = $scope.wpm_timer_value / 60;
+            var _total_minutes = ($scope.wpm_timer_value - TIMEOUT_SECONDS*auto_factor) / MINUTE_IN_SECONDS;
+            /*if(_total_minutes > 1.0)
+                _total_minutes = _total_minutes - 1;*/
             var _char_count = _text.length;
-            var _wpm = (_char_count / 5.0) / (_total_minutes - 1);
+            var _wpm = (_char_count / 5.0) / (_total_minutes);
             _wpm = Math.round(_wpm);
             $scope.records[_time]["wpm"] = _wpm;
             $scope.wpm_timer_value = 0;
@@ -101,14 +111,27 @@ myApp.controller('MyController', ['$scope', '$http', '$interval',
 
         }
 
+        /* Save data
+            * "Web storage is more secure [than cookies], and large amounts of data can be stored locally,
+            * without affecting website performance." w3schools.com
+        */
         $scope.share = function () {
             $scope.share_code = Math.random().toString(36).slice(8);
             localStorage.setItem($scope.share_code, JSON.stringify($scope.records));
         }
+
+        $scope.submitNow = function () {
+            auto_factor = 0;
+            $interval.cancel(_timer);
+            $interval.cancel(_wpm_timer);
+            _wpm_timer = null;
+            analyze();
+        }
     }]);    // End of SCOPE
 
-// Custom directive
-// Reference: https://stackoverflow.com/questions/30946624/how-to-use-ngchange-on-a-div-or-something-similar
+/* Custom directive
+* Reference: https://stackoverflow.com/questions/30946624/how-to-use-ngchange-on-a-div-or-something-similar
+*/
 myApp.directive('contenteditable', function () {
     return {
         require: 'ngModel',
@@ -116,11 +139,11 @@ myApp.directive('contenteditable', function () {
         link: function (scope, elm, attr, ngModel) {
 
             function updateViewValue() {
-                ngModel.$setViewValue(this.textContent);
+                ngModel.$setViewValue(this.innerText);
             }
 
-            //Binding it to keyup, lly bind it to any other events of interest
-            //like change etc..
+            /* Binding editable div(alt for textarea) to keyup
+            */
             elm.on('keyup', updateViewValue);
 
             scope.$on('$destroy', function () {
